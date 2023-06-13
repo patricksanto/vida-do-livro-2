dd4255187a3857eb65bca2d46723d40e9914135b
Patrick Santos <patrick.sg.santos@gmail.com>
2023-06-13 10:25:50 -0700

    workshop page done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :workshop]
 
   def home
+  end
+
+  def workshop
+    @hide_navbar = true
   end
 end
