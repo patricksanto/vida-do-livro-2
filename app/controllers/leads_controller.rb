1d9e7680666d0e5d211a09132b466bedbe9522cc
Patrick Santos <patrick.sg.santos@gmail.com>
2023-07-10 16:07:39 -0700

    fix the logic behind the form to create or just update a pre existed lead in the data base

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -11,7 +11,8 @@ class LeadsController < ApplicationController
   # end
 
   def waiting_list
-    @lead = Lead.new(lead_params)
+    @lead = Lead.find_or_initialize_by(email: lead_params[:email])
+    @lead.assign_attributes(lead_params)
     @lead.tags << lead_params[:tags]
 
     # redirect_to root_path if consult_lead
@@ -44,7 +45,8 @@ class LeadsController < ApplicationController
   end
 
   def create
-    @lead = Lead.new(lead_params)
+    @lead = Lead.find_or_initialize_by(email: lead_params[:email])
+    @lead.assign_attributes(lead_params)
     @lead.tags << lead_params[:tags]
 
     # redirect_to root_path if consult_lead
@@ -60,6 +62,7 @@ class LeadsController < ApplicationController
     request.body = { name: @lead.name, email: @lead.email, tags: @lead.tags }.to_json
 
     response = http.request(request)
+    puts JSON.parse(response.body)
 
     if response.is_a?(Net::HTTPSuccess)
       result = JSON.parse(response.body)
@@ -67,9 +70,29 @@ class LeadsController < ApplicationController
       flash[:notice] = 'Aproveite a masterclass!'
       redirect_to ofuturodolivro_masterclass_path(anchor: 'workshopform')
     else
-      if @lead.save
-        # redirect_to root_path, notice: 'Aproveite o workshop!'
-        p 'Lead salvo so na DB local'
+      if (JSON.parse(response.body)["errors"]["error_type"] == "EMAIL_ALREADY_IN_USE")
+
+        encoded_email = URI.encode_www_form_component(@lead.email)
+        url = URI("https://api.rd.services/platform/contacts/email:#{encoded_email}/tag")
+
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+
+        request = Net::HTTP::Post.new(url)
+        request["accept"] = 'application/json'
+        request["content-type"] = 'application/json'
+        request['Authorization'] = "Bearer #{refresh_token}"
+        request.body = "{\"tags\":[\"videoaula_johnbthompson_jun23\"]}"
+
+        response2 = http.request(request)
+        puts response.read_body
+
+        if response2.is_a?(Net::HTTPSuccess)
+          flash[:notice] = 'Aproveite a masterclass!'
+          redirect_to ofuturodolivro_masterclass_path(anchor: 'workshopform')
+        else
+          render :new, status: :unprocessable_entity
+        end
       else
         render :new, status: :unprocessable_entity
       end
