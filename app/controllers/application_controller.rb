7af69d34120fcb677e219b5aa6016d9a9312848a
Patrick Santos <patrick.sg.santos@gmail.com>
2023-07-14 12:45:03 -0700

    redirect user to root path when 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  rescue_from ActionController::RoutingError, with: :redirect_to_root
+
+  def redirect_to_root
+    redirect_to root_path
+  end
 end
