f57ffa829a80ccfd71476d283d02666c1a130b90
Patrick Santos <patrick.sg.santos@gmail.com>
2023-07-21 16:37:02 -0700

    validations for cellphone-number working now

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -2,5 +2,6 @@ class Lead < ApplicationRecord
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :name, presence: true
   # validates :cellphone_number, format: { with: /\A\d{10,12}\z/, message: "Insira um número de telefone válido" }
-
+  validates :cellphone_number, length: { in: 10..12 }, allow_blank: true
+  validates_numericality_of :cellphone_number, only_integer: true, allow_blank: true, length: { in: 10..12 }
 end
